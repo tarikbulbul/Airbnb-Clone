313d023bbe129926d04dd6bddfe04debe4607b04
Tarik Bulbul <tbulbul54@gmail.com>
2022-09-09 19:13:54 +0300

    Add Search Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from "./components/Home/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Home/Footer";
+import SearchPage from "./components/Search/SearchPage";
 import {
   BrowserRouter,
   Routes,
@@ -13,6 +14,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/search" element={<SearchPage/>} />
         <Route path="/" element={<Home/>} />
       </Routes>
       <Footer />
